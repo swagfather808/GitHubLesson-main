f5fa099537a1669c05416fda52df8a6c84bd03dd
swagfather808 <rkringor@gmail.com>
2024-11-26 11:44:37 -1000

    Add background to second page button

--- screens/SecondPage.js
+++ screens/SecondPage.js
@@ -3,10 +3,18 @@ import MainButton from "../components/MainButton";
 
 function SecondPage(props) {
   return (
+    <View style={styles.container}>
    <MainButton onPress = {props.changeScreenBack}> mjdagoat </MainButton>
+   </View>
   );
+ 
 }
 
 export default SecondPage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'gray',
+  },
+});
