3e58c9454b6f52c77b0c6666e9d6795f63fbf081
sky <imsky718@gmail.com>
2024-11-26 11:40:58 -1000

    Color Sheet Added

---
+++ components/Colors.js
@@ -0,0 +1,10 @@
+const Colors = {
+    accent500: '#119911',
+    yellow: 'FFFF00',
+    primary900: '#220066',
+    primary1: '#4e0329',
+    white: '#FFFFFF',
+    myGray: '#aaaaaa',
+}
+
+export default Colors;
