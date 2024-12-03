56a96c6f386f0ddab40ef206715332caabf2b9ee
swagfather808 <rkringor@gmail.com>
2024-12-03 13:02:04 -1000

    Got rid of useless function

--- App.js
+++ App.js
@@ -6,64 +6,60 @@ import ThirdPage from './screens/ThirdPage'
 import LebronPage from './screens/LebronPage'
 
 export default function App() {
-  const [currentPage,setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
 
-  function changeToLebron() {
-    setCurrentPage(3)
-  }
-  
   function changeToThird() {
     setCurrentPage(3)
   }
 
-  function changeScreen2(){
+  function changeToThird() {
+    setCurrentPage(3)
+  }
+
+  function changeScreen2() {
     setCurrentPage(2)
   }
 
-  function changeToSecond(){
 
-  function changeScreen(){
-     setCurrentPage(1)
+  function changeScreen() {
+    setCurrentPage(1)
   }
 
-  function changeScreenBack(){
+  function changeScreenBack() {
     setCurrentPage(0)
   }
- 
 
-  let screen = <FirstPage changeScreen={changeScreen} changeScreen2 = {changeScreen2} changeToThird = {changeToThird}/>
 
-  
-  
+  let screen = <FirstPage changeScreen={changeScreen} changeScreen2={changeScreen2} changeToThird={changeToThird} />
 
-  if(currentPage === 1){
-    screen = <SecondPage changeScreenBack={changeScreenBack}/>
+
+
+
+  if (currentPage === 1) {
+    screen = <SecondPage changeScreenBack={changeScreenBack} />
   }
 
-  if(currentPage === 2)
-  {
-    screen = <ThirdPage changeScreenBack={changeScreenBack}/>
+  if (currentPage === 2) {
+    screen = <ThirdPage changeScreenBack={changeScreenBack} />
   }
 
-  if(currentPage === 3) {
-    screen = <LebronPage changeScreenBack = {changeScreenBack} />
-  if(currentPage === 3){
-    screen = <LebronPage changeScreenBack={changeScreenBack}/>
+  if (currentPage === 3) {
+    screen = <LebronPage changeScreenBack={changeScreenBack} />
+
+    console.log(screen);
+    return (
+      <SafeAreaView style={styles.container}>
+        {screen}
+      </SafeAreaView>
+    );
   }
 
-  console.log(screen);
-  return (
-    <SafeAreaView style={styles.container}>
-      {screen}
-    </SafeAreaView>
-  );
+  const styles = StyleSheet.create({  
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  })
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
