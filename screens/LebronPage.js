7bc24404f9b01c8631339203288bdf74d173331e
swagfather808 <rkringor@gmail.com>
2024-12-04 10:29:36 -1000

    Create a button that works and makes the thing move!

--- screens/LebronPage.js
+++ screens/LebronPage.js
@@ -1,13 +1,59 @@
-import { View, Image } from 'react-native'
+import { View, Image, StyleSheet, Button } from 'react-native'
 import MainButton from '../components/MainButton';
+import Animated, {
+    useSharedValue,
+    withTiming,
+    useAnimatedStyle,
+    Easing,
+} from 'react-native-reanimated';
 
 function LebronPage(props) {
+    const randomWidth = useSharedValue(10)
+
+    const config = {
+        duration: 1000,
+        easing: Easing.bezier(0.5, 0.01, 0, 0.1)
+    }
+
+    const style = useAnimatedStyle(() => {
+        return {
+            width: withTiming(randomWidth.value, config)
+        }
+
+    })
+
+
+
     return (
-        <View>
+        <View style={styles.container}>
+            <Animated.View style={[styles.box, style]} />
+            <Button
+                title='toggle'
+                onPress={() => {
+                    randomWidth.value = Math.random() * 550;
+                }}
+            />
             <MainButton onPress={props.changeScreenBack}> I love Lebron</MainButton>
+
         </View>
     );
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    box: {
+        width: 100,
+        height: 80,
+        backgroundColor: 'green',
+        margin: 30,
+    }
+
+})
 
 export default LebronPage; 
